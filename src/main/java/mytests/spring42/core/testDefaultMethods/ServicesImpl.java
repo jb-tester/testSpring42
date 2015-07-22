56b95ed4120f612b1f74b5e0d05eac23f0701db9
irina <irina@petrovskaya.me>
2015-07-22 13:12:48 +0300

    template

--- src/main/java/mytests/spring42/core/testDefaultMethods/ServicesImpl.java
+++ src/main/java/mytests/spring42/core/testDefaultMethods/ServicesImpl.java
@@ -2,6 +2,7 @@ package mytests.spring42.core.testDefaultMethods;
 
 /**
  * Created by irina on 7/22/2015.
+ * project: testSpring42
  */
 @SuppressWarnings("unused")
 class ServicesImpl implements Service1, Service2 {
